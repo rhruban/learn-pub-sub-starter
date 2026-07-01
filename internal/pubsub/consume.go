ff3c020a660c3121ebf14ea11d76ffdeca324183
rh <rhruban@gmail.com>
2026-07-01 09:26:52 -0600

    added pause handling to the client

--- internal/pubsub/consume.go
+++ internal/pubsub/consume.go
@@ -1,6 +1,7 @@
 package pubsub
 
 import (
+	"encoding/json"
 	"fmt"
 
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -45,4 +46,50 @@ func DeclareAndBind(
 	}
 
 	return ch, queue, nil
+}
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType, // an enum to represent "durable" or "transient"
+	handler func(T),
+) error {
+	ch, queue, err := DeclareAndBind(
+		conn,
+		exchange,
+		queueName,
+		key,
+		queueType,
+	)
+	if err != nil {
+		return fmt.Errorf("Could not declare and bind queue: %v", err)
+	}
+
+	pages, err := ch.Consume(queue.Name, "", false, false, false, false, nil)
+	if err != nil {
+		return fmt.Errorf("Could not consume messages: %v", err)
+	}
+
+	unmarshaller := func(data []byte) (T, error) {
+		var target T
+		err := json.Unmarshal(data, &target)
+		return target, err
+	}
+
+	go func() {
+		defer ch.Close()
+		for msg := range pages {
+			target, err := unmarshaller(msg.Body)
+			if err != nil {
+				fmt.Printf("Could not unmarshal message: %v\n", err)
+				continue
+			}
+			handler(target)
+			msg.Ack(false)
+		}
+	}()
+
+	return nil
 }
